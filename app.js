a1dc0e6314798a4770e6898fb7459026f250e57d
mouad13 <mouad.rifaielmoubarik@gmail.com>
2017-03-10 11:21:57 +0100

    fin exr nombre my

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
-var solution = (Math.random()* 3);
-console.log(solution);
-console.log( Math.round(solution) )	
+var solution = Math.floor(Math.random()* 4);
+   
+   console.log(solution);
+
+   for (var i = 0; i < 3; i++) {
+  
+var response = prompt("choisir un chiffre entre 0 et 3")	
+  
+  console.log(response);
+   	
+   }
+
+  if (response == solution) {
+  	console.log("bravo")
+  }
+
+else if (response > solution) {
+	console.log("plus grand")
+   
+   }
+   if (response < solution) {
+   	console.log("plus petit")
+   }
